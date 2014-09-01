075af3793559d380c139a8867beb452a2d769ed5
Felipe Bazzarella <fbazzarella@gmail.com>
2014-09-01 15:08:22 -0300

    Método para exibir um resumo dos erros de importação

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,4 +3,11 @@ FactoryGirl.define do
     username 'johndoe'
     password 'secret'
   end
+
+  factory :import_error do
+    file_name 'file'
+    line_number 1
+    line_string 'string'
+    error_messages 'error'
+  end
 end
--- spec/models/import_error_spec.rb
+++ spec/models/import_error_spec.rb
@@ -7,4 +7,19 @@ RSpec.describe ImportError, type: :model do
   it { should validate_presence_of(:error_messages) }
 
   it { should validate_numericality_of(:line_number) }
+
+  describe '.resume' do
+    subject { described_class.resume }
+
+    before do
+      %w(import error error).each do |e|
+        create(:import_error, error_messages: e)
+      end
+    end
+
+    it { expect(subject).to be_a(Hash) }
+    it { expect(subject['import']).to eql(1) }
+    it { expect(subject['error']).to eql(2) }
+    it { expect(subject['other']).to be_nil }
+  end
 end

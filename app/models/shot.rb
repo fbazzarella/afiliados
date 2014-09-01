b0dba17c2a4beccd2a17ad201812943db26d1977
Felipe Bazzarella <fbazzarella@gmail.com>
2014-09-01 18:16:44 -0300

    Delegando verified_at de Email em Shot

--- app/models/shot.rb
+++ app/models/shot.rb
@@ -2,5 +2,7 @@ class Shot < ActiveRecord::Base
   belongs_to :email
   belongs_to :campaign
 
+  delegate :verified_at, to: :email
+
   validates :email_id, :campaign_id, presence: true
 end
--- spec/models/shot_spec.rb
+++ spec/models/shot_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Shot, type: :model do
   it { should belong_to(:email) }
   it { should belong_to(:campaign) }
 
+  it { should delegate_method(:verified_at).to(:email) }
+
   it { should validate_presence_of(:email_id) }
   it { should validate_presence_of(:campaign_id) }
 end

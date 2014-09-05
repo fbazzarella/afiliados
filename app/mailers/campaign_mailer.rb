5efb8b55799d41ecddd473524b9d2feed2381573
Felipe Bazzarella <fbazzarella@gmail.com>
2014-09-04 23:30:22 -0300

    Criando a mensagem do anúncio

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -1,13 +1,13 @@
 class CampaignMailer < ActionMailer::Base
   include SendGrid
   
-  default from: 'Felipe Bazzarella <cursos@bazzarella.com>'
+  default from: 'Felipe Bazzarella <fbazzarella@gmail.com>'
 
   def shot(shot)
     @shot = shot
 
     sendgrid_unique_args({shot_id: @shot.id})
 
-    mail(to: @shot.email.address, subject: @shot.campaign.name)
+    mail(to: @shot.email.address, subject: 'Oopa! Dá uma olhada :D')
   end
 end
--- spec/mailers/campaign_mailer_spec.rb
+++ spec/mailers/campaign_mailer_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe CampaignMailer, type: :mailer do
 
     subject { CampaignMailer.shot(shot) }
 
-    it { expect(subject.header['From'].to_s).to be_eql('Felipe Bazzarella <cursos@bazzarella.com>') }
+    it { expect(subject.header['From'].to_s).to be_eql('Felipe Bazzarella <fbazzarella@gmail.com>') }
     it { expect(subject.header['To'].to_s).to be_eql(shot.email.address) }
-    it { expect(subject.subject).to be_eql(shot.campaign.name) }
+    it { expect(subject.subject).to be_eql('Oopa! Dá uma olhada :D') }
     it { expect(subject.body.to_s).to_not be_empty }
     it { expect{subject.deliver}.to change(ActionMailer::Base.deliveries, :size).by(1) }
   end

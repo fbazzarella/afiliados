08283cf56a929ec5490dd705c2586ff220b751d0
Felipe Bazzarella <fbazzarella@gmail.com>
2015-11-19 08:37:37 -0200

    Pequena melhoria nos testes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,7 +11,7 @@ Dir[File.expand_path('../support/*', __FILE__)].each { |f| require f }
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
-  config.extend  ControllerMacros,    type: :controller
+  config.extend  HelperMethods
 
   config.use_transactional_fixtures = true
   config.formatter = Fuubar
--- spec/support/helper_methods.rb
+++ spec/support/helper_methods.rb
@@ -1,9 +1,16 @@
-module ControllerMacros
+module HelperMethods
   def login!
     before do
       @request.env["devise.mapping"] = Devise.mappings[:user]
       @current_user = create(:user)
       sign_in @current_user
+    end
+  end
+
+  def clean_lists!
+    after do
+      files = File.join(ListHandler::LISTS_PATH, '*')
+      FileUtils.rm(Dir.glob(files))
     end
   end
 end
